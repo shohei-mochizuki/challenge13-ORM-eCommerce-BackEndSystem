cb94d18be7a30ab2ce9b3f43d48921934bac3fb9
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:14:31 -0500

    Add comments to seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,12 @@
+// Specify a file for each seeding
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection'); // sequelize: Connection to MySQL2
 
+// Sync data base and seeds data of each item (categories, products, ...) one by one
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
