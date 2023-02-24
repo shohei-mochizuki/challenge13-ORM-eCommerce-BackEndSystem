c5d4209a9b8e0b711e2c9c2a0f71bddba38b8ce4
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:20:41 -0500

    Add comments to Category.js

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,7 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define columns following the requirements/instructions
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
