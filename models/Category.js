2a0b5741274aa81a434d83ca4f9dab41b08be968
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:23:29 -0500

    Add comments to Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,10 @@
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Import our database connection from connection.js
 const sequelize = require('../config/connection.js');
 
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
 Category.init(
