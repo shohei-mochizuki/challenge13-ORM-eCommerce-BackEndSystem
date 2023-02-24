bce2f3dba5da761987991025d10ce9497ba1371c
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:24:34 -0500

    Add comments to Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,15 @@
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Import our database connection from connection.js
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define columns following the requirements/instructions
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
