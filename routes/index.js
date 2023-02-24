c50b10e2babfebb470dddac8f12ebca6c9214e97
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:16:39 -0500

    Add comments to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+// When a wrong path is requested, this error message will be shown in Insomnia
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
