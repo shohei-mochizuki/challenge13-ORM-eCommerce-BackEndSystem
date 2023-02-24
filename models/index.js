4c3618a874d5d07f3b968242f68d4694e7ee42cc
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 16:09:46 -0500

    Fix onDelete settings

--- models/index.js
+++ models/index.js
@@ -4,26 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+  onUpdate: 'SET NULL'
+});
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'SET NULL',
-});
-
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
   through: {
     model: ProductTag
   }
 })
 
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag
   }
