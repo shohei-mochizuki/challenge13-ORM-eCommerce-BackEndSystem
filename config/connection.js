9c8b93e8ee12258f10f7401d531f2e1353bad7c3
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:19:02 -0500

    Add comments to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require('dotenv').config(); // dotenv: store sensitive data in .env file
 
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize'); // sequelize: Connection to MySQL2
 
-const sequelize = process.env.JAWSDB_URL
+const sequelize = process.env.JAWSDB_URL // JAWSDB_URL is for HEROKU
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This will be used in case of executing locally
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
