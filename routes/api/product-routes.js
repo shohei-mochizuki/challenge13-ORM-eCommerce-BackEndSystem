f4400909646fe7000be0fd572601c10722604e0c
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-23 22:58:01 -0500

    Add get route for /api/product/:id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,8 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
     try {
     const productData = await Product.findAll({ 
-    include: [{ model: Category }, { model: Tag }]});
+      include: [{ model: Category }, { model: Tag }]
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -17,9 +18,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
