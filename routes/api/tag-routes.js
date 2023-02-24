3b6e28f13763ff20e9b49ff3ffb4ce1e276f8613
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:28:44 -0500

    Add comments to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,26 +1,24 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+// "/api/tags" endpoint
 
+// GET - View all tags
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({ 
-    include: [{ model: Product }]});
+    include: [{ model: Product }]}); // View associate product data as well
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// GET - View specific tag
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product}]
+      include: [{ model: Product}] // View associate product data as well
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
