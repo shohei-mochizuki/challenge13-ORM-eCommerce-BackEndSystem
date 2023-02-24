926445019a302324cd4c2921ee467a1b6bcbdf5d
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-23 23:42:45 -0500

    Update pur route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,7 +47,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
