69cccee00b91bbfdfe32fa8c63396662a08da983
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 01:25:55 -0500

    Add comments to ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,26 +1,30 @@
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Import our database connection from connection.js
 const sequelize = require('../config/connection');
 
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+// Set up fields and rules for ProductTag model
 ProductTag.init(
   {
-    // define columns
+    // Define columns following the requirements/instructions
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
-    product_id: {
+    product_id: { // Foreign key
       type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
       }
     },
-    tag_id: {
+    tag_id: { // Foreign key
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
